fc00bf4c329282cebd2b3118f4e4ea5d1bf020ae
Shaina G <shainagtherapy@gmail.com>
2025-09-08 13:08:08 -0700

    Setup - Homepage rendered

---
+++ main_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>STICKERBOOK COLLECTION</h1>')
