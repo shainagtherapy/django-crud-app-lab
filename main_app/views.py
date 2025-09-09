fa8ee735f14dc935d714d24f2a9ecebc60773b45
Shaina G <shainagtherapy@gmail.com>
2025-09-09 13:55:23 -0700

    Admin Access!

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-
-class Sticker:
-    def __init__(self, name, shape, brand, description):
-        self.name = name
-        self.shape = shape
-        self.brand = brand
-        self.description = description
-
-stickers = [
-    Sticker('Donatello', 'outline', 'TMNT', 'Purple Ninja-turtle'),
-    Sticker('Dolphins', 'outline', 'Lisa Frank', 'Dancing Dolphins'),
-    Sticker('Star', 'star', 'generic', 'Multi-colored star set'),
-    Sticker('Firestone', 'outline', 'Firestone Brewing', 'Firestone Brewing classic logo')
-]
+from .models import Sticker
 
 # Define the home view function
 def home(request):
@@ -22,4 +9,5 @@ def about(request):
     return render(request, 'about.html')
 
 def sticker_index(request):
+    stickers = Sticker.objects.all()
     return render(request, 'stickers/index.html', {'stickers': stickers})
